5f9c6ea0eca6ededb3f9bd0bb332d021881ad414
chenxiaocehng <583188551@qq.com>
2020-05-22 15:47:45 +0800

    初始化代码

---
+++ stock-demo/src/test/java/com/linjingc/stockdemo/StockDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.linjingc.stockdemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StockDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
