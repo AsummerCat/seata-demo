66a64eb4ffc490c098989a9601820a9a09a5372c
chenxiaocehng <583188551@qq.com>
2020-05-22 18:30:59 +0800

    异常报错无法使用

--- pay-demo/src/main/java/com/linjingc/paydemo/PayDemoApplication.java
+++ pay-demo/src/main/java/com/linjingc/paydemo/PayDemoApplication.java
@@ -1,12 +1,16 @@
 package com.linjingc.paydemo;
 
+import com.linjingc.paydemo.config.DataSourceProxyAutoConfiguration;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 import org.springframework.cloud.openfeign.EnableFeignClients;
+import org.springframework.context.annotation.Import;
 
-@SpringBootApplication
 @EnableFeignClients
+@SpringBootApplication(exclude = DataSourceAutoConfiguration.class)
+@Import(DataSourceProxyAutoConfiguration.class)
 @MapperScan("com.linjingc.paydemo.dao")
 public class PayDemoApplication {
 
